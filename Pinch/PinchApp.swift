1dc74fac7c981f565318ee540e901d57fe502308
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-08-08 05:45:38 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by minkyuLee on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
